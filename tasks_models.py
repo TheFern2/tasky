4d8cfc40e82a833606dd317da2fbc41507d44871
kodaman2 <fredz0003@gmail.com>
2019-07-24 00:20:32 -0500

    Copy File/Folder Done

--- tasks_models.py
+++ tasks_models.py
@@ -1,11 +1,20 @@
 import shutil, os
+from tasky import log
+from tasky import now
 
-# task types: copy, ping, open, compare, zip, del, ftp...
+# task types: copy, ping, exec, compare, zip, del, ftp...
+
+# MultipleTask
+class MultipleTask(object):
+    def __init__(self, section_name, task_type, tasks):
+        self.section_name = section_name
+        self.task_type = task_type
+        self.tasks = tasks
 
 # Task Base Class
 class Task(object):
     def __init__(self, section_name):
-        self.section_name = ''
+        self.section_name = section_name
         self.task_type = ''
         self.task_name = ''
         self.task_cmd = ''
@@ -15,7 +24,7 @@ class Task(object):
 CopyTask subclass of Task:
 
 Class to instantiate tasks that require copying files or folders.
-Source, and Dest can be multiple delimited by a comma at the input side
+Source, and Dest can be multiple used as json with configparse in tandem
 '''
 class CopyTask(Task):
     def __init__(self, source, dest):
@@ -29,12 +38,28 @@ class CopyTask(Task):
     def copy_folder(self):
         for source in self.source:
             for dest in self.dest:
-                copytree2(self.source, self.dest)
+                try:
+                    copytree2(source, dest)
+                except Exception as e:
+                    print("Did not find file") # this needs to be logged!
+                    log.write("%s Copy Folder Error %s\n" % (now.strftime("%c"), e))
     
     def copy_file(self):
         for source in self.source:
             for dest in self.dest:
-                shutil.copy(self.source, self.dest)
+                try:
+                    shutil.copy(source, dest)
+                except Exception as e:
+                    print("Did not find file") # this needs to be logged!
+                    log.write("%s Copy File Error %s\n" % (now.strftime("%c"), e))
+
+
+class ExecTask(Task):
+    pass
+
+
+class CompareTask(Task):
+    pass
 
 
 # Custom copytree2 in case folder is already created
--- tasky.py
+++ tasky.py
@@ -5,6 +5,8 @@ import argparse
 # import subprocess
 import shutil
 from tasks_models import *
+import json
+import datetime
 
 
 config = configparser.ConfigParser()
@@ -12,11 +14,13 @@ arg_parser = None
 args = None
 single_tasks = []
 mutiple_tasks = []
+log = open('log.txt', 'a+')
+now = datetime.datetime.now()
 
-def load_tasks():
+def load_single_tasks():
+    global config
     global single_tasks
-    config.read('tasks.ini')
-    
+
     for task in config.sections():
         
         task_type = config[task]['task_type']
@@ -26,8 +30,10 @@ def load_tasks():
             task_cmd = config[task]['task_cmd']
 
             if task_cmd == 'copy_folder':
-                source = config[task]['source']
-                dest = config[task]['dest']
+                #source = config[task]['source']
+                source = json.loads(config.get(task, 'source'))
+                #dest = config[task]['dest']
+                dest = json.loads(config.get(task, 'dest'))
                 t = CopyTask(source, dest)
                 t.task_type = config[task]['task_type']
                 t.task_name = config[task]['task_name']
@@ -35,14 +41,29 @@ def load_tasks():
                 single_tasks.append(t)
 
             if task_cmd == 'copy_file':
-                source = config[task]['source']
-                dest = config[task]['dest']
+                source = json.loads(config.get(task, 'source'))
+                dest = json.loads(config.get(task, 'dest'))
                 t = CopyTask(source, dest)
                 t.task_type = config[task]['task_type']
                 t.task_name = config[task]['task_name']
                 t.task_cmd = config[task]['task_cmd']
                 single_tasks.append(t)
 
+def load_multiple_tasks():
+    global config
+    global mutiple_tasks
+
+    for task in config.sections():
+        
+        task_type = config[task]['task_type']
+
+        if task_type == 'multiple':
+            t = MultipleTask(task, task_type, single_tasks)
+            mutiple_tasks.append(t)
+
+def load_tasks():
+    load_single_tasks()
+    load_multiple_tasks()
 
 def arguments():
     global args
@@ -53,43 +74,84 @@ def arguments():
         Usage example:
 
         tasky = tasky.py or tasky.exe
-        tasky args
+        tasky args*
 
-        -m --multiple          : Get a list of multiple tasks
-        -s --single            : Get a list of single tasks
-        -pa --perform_all      : Perform all multiple tasks
-        -ps --perform_ind      : Perform all single tasks
-        -po --perform_one      : Performs one single task
-        -pm --perform_multiple : Performs one multiple task
-        -pn --perform_number   : Performs task by number
+        To perform one task by number:
+        tasky -sn 1
+
+        To perform one multiple task by number:
+        tasky -mn 1
+
         '''
     )
 
-    arg_parser.add_argument('-m', '--multiple', dest='multiple', action='store_true', help='get multiple tasks')
-    arg_parser.add_argument('-s', '--single', dest='single', action='store_true', help='get single tasks')
+    arg_parser.add_argument('-m', '--multiple', dest='multiple', action='store_true', help='multiple tasks')
+    arg_parser.add_argument('-s', '--single', dest='single', action='store_true', help='single tasks')
+    arg_parser.add_argument('-a', '--all', dest='all', action='store_true', help='all tasks')
+    arg_parser.add_argument('-p', '--print', dest='print', action='store_true', help='prints')
+    arg_parser.add_argument('-n', '--number', type=int)
     arg_parser.set_defaults(multiple=False)
     arg_parser.set_defaults(single=False)
+    arg_parser.set_defaults(print=False)
+    arg_parser.set_defaults(all=False)
     args = arg_parser.parse_args()
+
+
+def perform_single_task_by_number(task_number):
+    task = single_tasks[task_number]
+    perform_single_task_by_cmd(task)
+
+
+def perform_multiple_task_by_number(task_number):
+    m_task = mutiple_tasks[task_number]
+    tasks = m_task.tasks
+
+    for task in tasks:
+        perform_single_task_by_cmd(task)
+
+def perform_single_task_by_name(task_name):
+    pass
+
+# Add more tasks here to avoid code repetition
+def perform_single_task_by_cmd(task):
+    if task.task_cmd == 'copy_folder':
+        task.copy_folder()
+    if task.task_cmd == 'copy_file':
+        task.copy_file()
 
 def main():
     global args
+    global config
+    config.read('tasks.ini')
     load_tasks()
     arguments()
 
-    if args.multiple:
+    if args.multiple and args.print:
         print("Print multiple tasks")
-    if args.single:
-        print("Print single tasks")
-        # index - task_cmd - task_name
-        for task in single_tasks:
-            if task.task_cmd == 'copy_folder':
-                task.copy_folder()
-        #     if task.task_cmd == 'copy_file':
-        #         task.copy_file()
-        # for index, task in enumerate(single_tasks):
-        #     print(' - [%s] Task Name: %s Task Cmd: %s \n' % (index, task.task_name, task.task_cmd))
-    
-   
+        for index, task in enumerate(mutiple_tasks):
+            print(' - [%s] Task Name: %s\n' % (index, task.section_name)) # maybe add number of tasks
+    if args.single and args.print:
+        print("Single tasks")
+        for index, task in enumerate(single_tasks):
+            print(' - [%s] Task Name: %s Task Cmd: %s \n' % (index, task.task_name, task.task_cmd))
+    if args.single and args.number is not None:
+        if args.number < len(single_tasks) and args.number >= 0:
+            perform_single_task_by_number(args.number)
+        else:
+            print("Enter a task number with -n argument, without exceding single task list -s")
+    if args.single and args.number is None and not args.print:
+        print("-n argument is required with -t i.e -tn 2")
+    if args.multiple and args.number is not None:
+        if args.number < len(mutiple_tasks) and args.number >= 0:
+            perform_multiple_task_by_number(args.number)
+        else:
+            print("Enter a task number with -n argument, without exceding single task list -s")
+    if args.multiple and args.number is None and not args.print:
+        print("-n argument is required with -m i.e -mn 2")
+    if args.all and args.single:
+        pass # do all single tasks
+    if args.all and args.multiple:
+        pass # do all multiple tasks
 
 if __name__ == '__main__':
     main()
